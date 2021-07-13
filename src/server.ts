673e05ed4bce97a2dd903c3fa99089dba169a284
Renanvt <jonatan_rvs@hotmail.com>
2021-07-13 18:04:43 -0300

    creating primary route

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,18 @@ const app = express(); //Inicializa o express
 //Cria o servidor
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
+
+/**Tipos de métodos
+ * GET      => Buscar uma informação
+ * POST     => Inserir (criar) uma informação dentro da API
+ * PUT      => Alterar uma informação
+ * DELETE   => Remover um dado
+ * PATCH    => Alterar uma informação específica
+ */
+
+//Cria a primeira rota
+app.get("/test", (request, response) => {
+    //Request => Entrando
+    //Response => Saindo
+    return response.send("Olá NLW!"); //Retorna uma mensagem
+})
